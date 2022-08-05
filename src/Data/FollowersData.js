5a6787bbaf4e031c9e6c3973c0a9483104197d7e
Aakash Deep <67054227+DeepAakash@users.noreply.github.com>
2022-08-05 23:25:31 +0530

    Add files via upload

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,12 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers=[
+    {name: "Rishabh Garg", username: "RishabhGarg", img: img1},
+    {name: "Utkarsh Thakur", username: "Utti", img: img2},
+    {name: "Nishant Gupta", username: "NishantGupta", img: img3},
+    {name: "Ankush Singla", username: "AmkussBimro", img: img4},
+    {name: "Abhinav Gupta", username: "AbhinavGupta", img: img4},
+];
