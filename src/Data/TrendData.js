4402fd036dd59112c662f42d72f26ec1f0f82d23
Aakash Deep <67054227+DeepAakash@users.noreply.github.com>
2022-08-19 16:25:33 +0530

    Add files via upload

---
+++ src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "FirstDay@TIET",
+      shares: 91,
+    },
+    {
+      name: "Saturnalia",
+      shares: 215,
+    },
+    {
+      name: "TFF",
+      shares: 198,
+    },
+    {
+      name: "MrCool",
+      shares: 72,
+    },
+    {
+      name: "Everyones_favourite",
+      shares: 108,
+    },
+    {
+      name: "MsPicturesque",
+      shares: 157,
+    },
+  ];
+  
